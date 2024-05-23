10b92d57ad4dfca66b9d36e90516e0cf44e345a8
Raí Joia <megac154@gmail.com>
2024-05-22 22:53:14 -0300

    feat: reading file .txt with code for lexer

--- src/app/components/projects/lexing/lexing.component.ts
+++ src/app/components/projects/lexing/lexing.component.ts
@@ -1,12 +1,46 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { Token, TokenType } from '../../../../interfaces/token';
+import { Lexer } from '../../../../utils/lexer';
 
 @Component({
   selector: 'app-lexing',
   standalone: true,
-  imports: [],
+  imports: [NgIf, NgFor],
   templateUrl: './lexing.component.html',
-  styleUrl: './lexing.component.scss'
+  styleUrl: './lexing.component.scss',
 })
 export class LexingComponent {
+  fileContent: string | null = null;
+  tokens: Token[] = [];
 
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.fileContent = reader.result as string;
+        this.tokenizeFileContent();
+      };
+      reader.onerror = (error) => {
+        console.error('Error reading file:', error);
+      };
+
+      reader.readAsText(file);
+    }
+  }
+
+  tokenizeFileContent(): void {
+    if (this.fileContent) {
+      const lexer = new Lexer(this.fileContent);
+      this.tokens = lexer.tokenize();
+    }
+  }
+
+  getTokenTypeName(type: TokenType): string {
+    return TokenType[type];
+  }
 }
